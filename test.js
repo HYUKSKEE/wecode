99f475f2c4d056854601846dc339e069bb35ee26
unknown <20144523@s.scnu.ac.kr>
2022-08-12 10:45:15 +0900

    [ADD]짝수자연수구하는 함수

---
+++ test.js
@@ -0,0 +1,9 @@
+function evenNum (num) {
+const evenNumArr= [];
+        for(let i=0; i<51;i++){
+                if(i%2==0){
+                evenNumArr.push(i)
+                }
+}return evenNumArr;
+}
+
